b914363b4dd67e3d491d85289d81adc18a0c7531
Sebastian Seitz <bassistsagainstracists@gmx.net>
2021-09-06 21:35:45 +0200

    add recorder

--- public/recorder.js
+++ public/recorder.js
@@ -1,85 +1,64 @@
-export class Recorder {
-  constructor (synthie, settings) {
-    /**
-     * @type {import('./multi-osc-lfo').Synthie}
-     */
-    this.synthie = synthie
-    this.settings = settings
-    this.isRecording = false
-    this.startTime = null
-    this.endTime = null
-    this.frames = []
-  }
-
-  init () {
-    window.addEventListener('keydown', ({ shiftKey }) => {
-      if (shiftKey) {
-        this.startRecording()
-      }
-    })
-
-    window.addEventListener('keyup', ({ shiftKey }) => {
-      if (!shiftKey) {
-        this.stopRecording()
-      }
-    })
-
-    this.synthie.subscribe(this)
-  }
-
-  startRecording () {
-    this.isRecording = true
-    this.frames = []
-    this.startTime = this.synthie.currentTime
-  }
-
-  stopRecording () {
-    this.isRecording = false
-    this.endTime = this.synthie.currentTime
-    this.startPlayback()
-  }
-
-  startPlayback () {
-    const deltaTime = this.startTime - this.synthie.currentTime
-    const queue = this.frames.slice().reverse()
-    let current = queue.pop()
-
-    if (!current) {
-      return
-    }
-
-    const handle = window.setInterval(() => {
-      const { currentTime } = this.synthie
-
-      console.log(current.time - currentTime - deltaTime)
-      if (current && (current.time >= currentTime + deltaTime)) {
-        this.synthie.handleMessage(current.data, current.settings)
-        current = queue.pop()
-      }
-
-      if (!current) {
-        window.clearInterval(handle)
-        this.startPlayback()
-      }
-    })
-  }
-
-  handleEvent (event) {
-    if (!this.isRecording) {
-      return
-    }
-
-    this.frames.push({
-      data: event.data,
-      settings: this.settings.clone(),
-      time: this.synthie.currentTime
-    })
-  }
-}
+const THRESHOLD = 100
 
 /**
  * @param {import('./multi-osc-lfo').Synthie} synthie
  */
 export function initRecorder (synthie) {
+  const destination = synthie.context.createMediaStreamDestination()
+  const recorder = new MediaRecorder(destination.stream)
+  const reader = new FileReader()
+  const chunks = []
+  let started = null
+  let source = null
 
+  synthie.compressor.connect(destination)
+
+  window.addEventListener('keydown', ({ shiftKey }) => {
+    if (!shiftKey || started) {
+      return
+    }
+
+    started = Date.now()
+    recorder.start()
+
+    if (source) {
+      source.disconnect()
+      source.stop()
+      source = null
+    }
+  })
+
+  window.addEventListener('keyup', ({ shiftKey }) => {
+    if (!shiftKey && started) {
+      recorder.stop()
+    }
+  })
+
+  recorder.addEventListener('dataavailable', event => {
+    chunks.push(event.data)
+  })
+
+  recorder.addEventListener('stop', () => {
+    const parts = chunks.splice(0)
+
+    if (Date.now() > started + THRESHOLD) {
+      const blob = new Blob(parts, {
+        type: 'audio/ogg; codecs=opus'
+      })
+
+      reader.readAsArrayBuffer(blob)
+    }
+
+    started = null
+  })
+
+  reader.addEventListener('loadend', () => {
+    synthie.context.decodeAudioData(reader.result, buffer => {
+      source = synthie.context.createBufferSource()
+      source.buffer = buffer
+      source.loop = true
+      source.connect(synthie.context.destination)
+      source.start()
+    }, console.error)
+  })
 }
